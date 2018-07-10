97b77c9f795880d1b400687db1ca4932a11c93eb
Patrick Laxton <patrick.laxton@protonmail.com>
2018-07-10 16:03:22 +0200

    initial commit

---
+++ xmonad.hs
@@ -0,0 +1,27 @@
+-- default desktop configuration for Fedora
+
+import System.Posix.Env (getEnv)
+import Data.Maybe (maybe)
+
+import XMonad
+import XMonad.Config.Desktop
+import XMonad.Config.Gnome
+import XMonad.Config.Kde
+import XMonad.Config.Xfce
+import XMonad.Config.Azerty
+import XMonad.Hooks.SetWMName
+
+main = do
+  session <- getEnv "DESKTOP_SESSION"
+  xmonad ( maybe desktopConfig desktop session ) { 
+    terminal    = "xterm -bg black -fg lightgrey",
+    modMask     = mod4Mask,
+    keys        = \c -> azertyKeys c <+> keys desktopConfig c,
+    startupHook = setWMName "LG3D"
+  }
+     
+desktop "gnome" = gnomeConfig
+desktop "kde" = kde4Config
+desktop "xfce" = xfceConfig
+desktop "xmonad-mate" = gnomeConfig
+desktop _ = desktopConfig
