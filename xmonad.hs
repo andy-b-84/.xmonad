6814397bb6c5d724e85aba3f19bcf7efdb9efb3c
Patrick Laxton <patrick.laxton@protonmail.com>
2018-08-09 14:41:14 +0200

    handles alsa

--- xmonad.hs
+++ xmonad.hs
@@ -11,6 +11,7 @@ import XMonad.Config.Azerty
 import qualified Data.Map as M
 import XMonad.Hooks.SetWMName
 import XMonad.Layout.NoBorders (noBorders)
+import Graphics.X11.ExtraTypes.XF86
 
 main = do
   session <- getEnv "DESKTOP_SESSION"
@@ -27,13 +28,16 @@ layout = tiled ||| Mirror tiled ||| Full
     tiled   = Tall nmaster delta ratio
     nmaster = 2
     ratio   = 1/2
-    delta = 3/100
+    delta   = 3/100
 
 myAzertyKeys x = M.union (myKeys x) (azertyKeys x)
 myKeys x = M.union (M.fromList (newKeys x)) (keys def x) 
 newKeys (XConfig {XMonad.modMask = modm}) = [
-    ((mod4Mask, xK_l), spawn "slock"),
-    ((mod4Mask, xK_g), sendMessage Expand)
+      ((mod4Mask, xK_l            ), spawn "slock")
+    , ((mod4Mask, xK_g            ), sendMessage Expand)
+    , ((0, xF86XK_AudioMute       ), spawn "amixer set Master toggle")
+    , ((0, xF86XK_AudioLowerVolume), spawn "amixer set Master -")
+    , ((0, xF86XK_AudioRaiseVolume), spawn "amixer set Master +")
   ]  
      
 desktop "gnome" = gnomeConfig
