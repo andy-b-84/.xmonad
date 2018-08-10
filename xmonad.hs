4567bbbea0ebd1cb774354304f0e300714d6b23f
Patrick Laxton <patrick.laxton@protonmail.com>
2018-08-10 16:18:11 +0200

    correct syntax for xrandr (generated by arandr) + using native hotkey instead of custom one

--- xmonad.hs
+++ xmonad.hs
@@ -39,14 +39,13 @@ toggledisplay = do
   screencount <- LIS.countScreens
   if screencount > 1
     then spawn "xrandr --output DP-3-1 --off"
-    else spawn "xrandr --output DP-3-1 --top-of eDP-1"
+    else spawn "~/.screenlayout/taf.sh"
 
 myAzertyKeys x = M.union (myKeys x) (azertyKeys x)
 myKeys x = M.union (M.fromList (newKeys x)) (keys def x) 
 newKeys (XConfig {XMonad.modMask = modm}) = [
       ((mod4Mask, xK_a               ), spawn "arandr")
     , ((mod4Mask, xK_c               ), spawn "code")
-    , ((mod4Mask, xK_d               ), toggledisplay)
     , ((mod4Mask, xK_f               ), spawn "firefox")
     , ((mod4Mask .|. shiftMask, xK_f ), spawn "firefox -P train")
     , ((mod4Mask, xK_g               ), sendMessage Expand)
@@ -55,6 +54,7 @@ newKeys (XConfig {XMonad.modMask = modm}) = [
     , ((mod4Mask, xK_s               ), spawn "spotify")
     , ((mod4Mask, xK_t               ), spawn "terminator")
     , ((mod4Mask .|. shiftMask, xK_t ), spawn "terminator -l recette")
+    , ((0, xF86XK_Display            ), toggledisplay)
     , ((0, xF86XK_AudioMute          ), spawn "amixer set Master toggle")
     , ((0, xF86XK_AudioLowerVolume   ), spawn "amixer -c 0 set Master 1dB-")
     , ((0, xF86XK_AudioRaiseVolume   ), spawn "amixer -c 0 set Master 1dB+")
